5aff8fd9da480ff24081e9a57fef10080f2945a1
kr1ta <justktita@mal.ru>
2025-04-13 11:46:46 +0300

    added /user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,25 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\View\View;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
-  /**
-  * Show the profile for a given user.
-  */
-  public function show(string $id)
-{
-    $user = User::findOrFail($id);
-    $allUsers = User::all();
+  public function show(Request $request)
+  {
+    $user = $request->user();
 
-    // Формируем текстовый ответ
-    $text = "User: " . $user->name . "\n";
-    $text .= "Success: true\n";
-    $text .= "All users: " . $allUsers->pluck('name')->implode(', ');
-
-    return response($text, 200)
-        ->header('Content-Type', 'text/plain');
-}
+    return response()->json([
+      'id' => $user->id,
+      'name' => $user->name,
+      'email' => $user->email,
+      'created_at' => $user->created_at,
+      'updated_at' => $user->updated_at,
+    ]);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TokenController;
+use App\Http\Controllers\UserController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/user', [UserController::class, 'show']);
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/update-password', [AuthController::class, 'updatePassword']);
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount']);
